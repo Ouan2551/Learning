f38e043ae546f649000ddd342a8b1e05d218dfe6
Ouan2551 <Nannaphat2110@gmail.com>
2025-01-04 11:40:17 +0700

    -

--- python/2.Python_Variables.py
+++ python/2.Python_Variables.py
@@ -15,3 +15,19 @@ print(type(b))
 # output
 print(x, y, a, b) # output multiple data in one line code
 print(y + a + b) # warning use same data type
+print('Hello', 'World') # this will output "Hello World"
+
+# global variable
+k = 'awesome' # this is global variable
+def myfunc():
+    k = 'fantastic' # this is local variable
+
+myfunc()
+print('Python is ' + k) # this command will output global
+
+def my_function():
+    global g
+    g = "fantastic"
+
+my_function()
+print("Python is " + g)
