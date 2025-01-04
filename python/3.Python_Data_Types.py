aee0f4c5068f773e1dde92b7975de0ec10fd560f
Ouan2551 <Nannaphat2110@gmail.com>
2025-01-04 12:21:12 +0700

    -

---
+++ python/3.Python_Data_Types.py
@@ -0,0 +1,14 @@
+a = "hello" # string
+b = 'h' # char
+c = 20 # integer
+d = 1.5 # float
+e = 1j # complex
+f = ["apple", "banana", "cherry"] # list
+g = ("apple", "banana", "cherry") # typle
+h = range(6) # range
+i = {"name" : "John", "age" : 36} # dict
+j = {"apple", "banana", "cherry"} # set
+k = frozenset({"apple", "banana", "cherry"}) # frozenset 
+l = True # bool
+m
+n
