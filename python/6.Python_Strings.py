f322adc3ff32c8e10a8a857408ab440bbc82e38b
Ouan2551 <Nannaphat2110@gmail.com>
2025-01-06 22:56:02 +0700

    -

---
+++ python/6.Python_Strings.py
@@ -0,0 +1,31 @@
+# single quote same as double quote in this case like this
+print(type("Hello"))
+print(type('Hello'))
+
+# normal string
+a = "hello"
+print(a)
+
+# multiple string (many line use triple quote)
+b = """today i will teach you how to cooking pasta
+and trick how to use pan"""
+print(b)
+
+# string is array
+# array is a room store value and reach by using index
+c = "Hello world"
+size = len(c) # use len() to get size string
+for i in range(0, size, 1):
+    print(c[i])
+
+# check string (use command "in")
+txt = "The best things in life are free!"
+print("free" in txt) # output true or false
+# or use with if else
+if "free" in txt:
+    print("Yes, 'free' is present.")
+
+# and have command "not in"
+print("expensive" not in txt)
+if "expensive" not in txt:
+    print("No, 'expensive' is NOT present.")
