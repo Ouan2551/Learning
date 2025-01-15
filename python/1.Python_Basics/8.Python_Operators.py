605bca094b2de50e7db7f46db7ebfee350e2f9fa
Ouan2551 <Nannaphat2110@gmail.com>
2025-01-15 23:38:20 +0700

    -

---
+++ python/1.Python_Basics/8.Python_Operators.py
@@ -0,0 +1,4 @@
+# in this lesson i will write only not common used for me
+print(10.5/2) # normal division
+print(10.5//2) # floor division
+# use "//" divide number will get result in integer data type
