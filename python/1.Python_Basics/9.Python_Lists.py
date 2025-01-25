b3dc8b8228fdf229cc079d49dfa2a08eab31980f
Ouan2551 <Nannaphat2110@gmail.com>
2025-01-25 10:13:30 +0700

    -

--- python/1.Python_Basics/9.Python_Lists.py
+++ python/1.Python_Basics/9.Python_Lists.py
@@ -1,3 +1,9 @@
+# python collections arrays have 4 collecting data types
+# 1. list => change and order allow duplicate members
+# 2.Tuple = can't change value allow duplicate members
+# 3.Set => unordered and unchanged no duplicate members
+# 4. Dictionary => like list no duplicate members
+
 # list => store multiple data in single variable
 This_list = ["apple", "banana", "cherry", "apple", "cherry"]
 print(This_list)
@@ -18,3 +24,23 @@ print(list_string)
 tuple_data = (1, 2, 3)
 list_data = list(tuple_data)
 print(list_data)
+
+# access list items => use index start from 1
+print(This_list[1]) # forward start from 0, 1, ...
+print(This_list[-1]) # backward start from -1, -2, ...
+print(This_list[2:4]) # start normal:before this number
+print(This_list[2:])
+
+# Change List Items => just use index and use new data to replace it
+This_list[0] = "watermelon"
+This_list[1:3] = ["lime", "lemon"]
+# upper code is replace value
+This_list.insert(3, "pumpkin") # not replace value just insert
+print(This_list)
+
+# Add List Items
+This_list.append("chill") # insert value in last member
+tropical = ["mango", "pineapple", "papaya"]
+This_list.extend(tropical) # insert another list in to member list
+print(This_list)
+
