84008b4460656f88cea184f2e808d5e0d790a336
🦜Nannaphat Songduong <121873082+Ouan2551@users.noreply.github.com>
2025-06-06 17:10:41 +0700

    -

--- Data_Structure/1.Arrays/1.DSA_Arrays.cpp
+++ Data_Structure/1.Arrays/1.DSA_Arrays.cpp
@@ -13,6 +13,33 @@ int main()
     }
     std::cout << lowest << '\n';
 
-    // bubble sort
+    // bubble sort => sort from lowest to highest
+    int nums1[5] = {1, 20, 500, 35, 25};
+    int len = sizeof(nums1) / sizeof(nums1[0]);
+    for (int i = 0; i < len - 1; i++)
+    {
+        for (int j = 0; j < len - i - 1; j++)
+        {
+            if (nums1[j] > nums1[j+1])
+            {
+                int temp = nums1[j];
+                nums1[j] = nums1[j+1];
+                nums1[j+1] = temp;
+            }
+        }
+    }
+    for (int i = 0; i < len; i++)
+    {
+        std::cout << nums1[i] << ' ';
+    }
+    
+    // selection sort => find lowest and move to the front
+    int nums2[] = {7, 12, 9, 11, 3, 5, 20};
+    len = sizeof(nums2) / sizeof(nums2[0]);
+    for (int i = 0; i < len; i++)
+    {
+        
+    }
+    
     return 0;
 }
