d916b736ed166a491e89e6d73c57d04da74ba038
🦜Nannaphat Songduong <121873082+Ouan2551@users.noreply.github.com>
2025-06-08 23:46:43 +0700

    -

---
+++ Data_Structure/1.Arrays/2.DSA_Linked_list.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+int main()
+{
+    std::ios::sync_with_stdio(0);std::cin.tie(0);std::cout.tie(0);
+    // Linked list => node that connect together each node contain data and pointer
+    return 0;
+}
