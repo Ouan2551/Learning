0f4cb4bfb90da080e37f087be6a68d89e08fa135
🦜Nannaphat Songduong <121873082+Ouan2551@users.noreply.github.com>
2025-06-17 19:25:08 +0700

    -

--- Data_Structure/1.Arrays/2.DSA_Linked_list.cpp
+++ Data_Structure/1.Arrays/2.DSA_Linked_list.cpp
@@ -3,5 +3,12 @@ int main()
 {
     std::ios::sync_with_stdio(0);std::cin.tie(0);std::cout.tie(0);
     // Linked list => node that connect together each node contain data and pointer
+    // type of linked list
+    // 1) singly liked list => only one address to next node
+
+    c
+
+    // 2) doubly linked list => has 2 node that next and previous node
+    // 3) circular liked list => first node (head) connect to last node (tail)
     return 0;
 }
