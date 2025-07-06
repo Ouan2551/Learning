25c06aa91f0070089f38b17aaafbcb7ca98361bf
🦜Nannaphat Songduong <Nannaphat2110@gmail.com>
2025-07-06 23:40:32 +0700

    -

---
+++ assembly/1.asm_intro.asm
@@ -0,0 +1,9 @@
+; basic computer 1(on), 0(off)
+; size of data type in assembly
+; word = 2 byte (32 bit) item
+; double_word = 8 byte (64 bit) item
+; paragraph = 16 byte (128 bit) area
+; kilobyte = 1024 bytes
+; Megabyte = 1,048,576 bytes
+
+; binary number system
