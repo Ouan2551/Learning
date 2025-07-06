e86ad7feecebafce41d8cf752a59d7a086870168
🦜Nannaphat Songduong <Nannaphat2110@gmail.com>
2025-07-06 23:16:28 +0700

    -

--- assembly/source_code.asm
+++ assembly/source_code.asm
@@ -1,15 +1,42 @@
+default rel   ; Use RIP-relative addressing by default
+; Data section
 section .data
-    message db 'Hello, world!', 0Ah ; Define a string with newline (0Ah)
-
+    msg db "Hello, World!", 13, 10  ; Message with CR+LF
+    msglen equ $ - msg              ; Length of message
+    STD_OUTPUT_HANDLE equ -11       ; Windows constant for stdout
+    NULL equ 0
+    
+section .bss
+    stdout resq 1                   ; Reserve space for handle
+    written resq 1                  ; Reserve space for bytes written
+; Import necessary Windows functions
+extern GetStdHandle
+extern WriteConsoleA
+extern ExitProcess
+; Code section
 section .text
-    global _start
-_start:
-    mov eax, 4  ; system call number for write
-    mov ebx, 1  ; file descriptor (stdout)
-    mov ecx, message  ; address of string to write
-    mov edx, 13  ; number of bytes to write
-    int 0x80  ; call kernel
-
-    mov eax, 1 ; system call number for exit
-    mov ebx, 0 ; exit status
-    int 0x80
+    global main
+main:
+    ; Set up stack frame
+    push    rbp
+    mov     rbp, rsp
+    sub     rsp, 32                ; Shadow space for function calls
+    ; Get handle to stdout
+    mov     ecx, STD_OUTPUT_HANDLE
+    call    GetStdHandle
+    mov     qword [rel stdout], rax
+    ; Write to console
+    mov     rcx, qword [rel stdout] ; Console handle
+    lea     rdx, [rel msg]         ; Message to write
+    mov     r8, msglen             ; Message length
+    lea     r9, [rel written]      ; Bytes written
+    push    NULL                   ; Overlapped
+    sub     rsp, 32                ; Shadow space
+    call    WriteConsoleA
+    add     rsp, 40                ; Clean up stack (32 + 8)
+    ; Exit program
+    xor     ecx, ecx               ; Exit code 0
+    call    ExitProcess
+    ; Function epilogue
+    leave
+    ret
