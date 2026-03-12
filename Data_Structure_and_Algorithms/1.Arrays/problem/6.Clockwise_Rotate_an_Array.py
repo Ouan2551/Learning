3dbc93cb62e5e9366edef3892434cdaa5aa1d295
🦜Nannaphat Songduong <Nannaphat2110@gmail.com>
2026-03-12 22:09:38 +0700

    -

---
+++ Data_Structure_and_Algorithms/1.Arrays/problem/6.Clockwise_Rotate_an_Array.py
@@ -0,0 +1,16 @@
+arr = [1, 2, 3]
+arr_new = []; arr_trail = []
+d = int(input())
+arr_trail = arr.copy()
+# for i in range(0, len(arr_trail), 1):
+#     print(arr_trail[i], end=" ");
+
+for i in range(0, d, 1):
+    arr_new.clear(); arr_new.append(arr_trail[len(arr)-1])
+    for j in range(0, len(arr)-1, 1):
+        arr_new.append(arr_trail[j]);
+    arr_trail = arr_new.copy();
+    print(arr_trail)
+
+for i in range(0, len(arr), 1):
+    print(arr_new[i], end=' ')
